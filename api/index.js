ef3aee746cba698d2211f77730d63edcb05cfeee
Rehaan Ansari <ansarirehaan811@gmail.com>
2024-01-29 19:53:39 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+try {
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("Succeeded to connected to MongoDB 🚀");
+    })
+    .catch((err)=>console.log(err));
+    
+} catch (error) {
+    console.log(error);
+}
+
 
 const app = express();
 
