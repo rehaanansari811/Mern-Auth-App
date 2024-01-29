f21000929938f83fbee41c26cddc770d2e7d2652
Rehaan Ansari <ansarirehaan811@gmail.com>
2024-01-29 21:38:03 +0530

    add a middleware and a function to handle errors

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -2,7 +2,7 @@ import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     console.log(req.body);
     const { username, password, email } = req.body;
     const hashedPassword = bcrypt.hashSync(password,10);
@@ -11,7 +11,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json({ message: "User created successfully" }); 
     } catch (error) {
-        res.status(500).json(error.message); 
-        
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -14,9 +14,17 @@ mongoose.connect(process.env.MONGODB_URL)
 
 const app = express();
 app.use(express.json())
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
-
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    });
+})
 app.listen(3000, () => {
     console.log("Server is listening on PORT  http://localhost:3000");
 })
