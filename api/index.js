6c1473ee79c7cc6fb38a8d91546f66f5f62b6b5b
Rehaan Ansari <ansarirehaan811@gmail.com>
2024-01-29 20:09:00 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -3,16 +3,11 @@ import express from "express";
 import mongoose from "mongoose";
 
 dotenv.config();
-try {
-    mongoose.connect(process.env.MONGODB_URL)
-    .then(()=>{
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
         console.log("Succeeded to connected to MongoDB 🚀");
     })
-    .catch((err)=>console.log(err));
-    
-} catch (error) {
-    console.log(error);
-}
+    .catch((err) => console.log(err));
 
 
 const app = express();
@@ -20,6 +15,6 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Welcome Rehaan');
 })
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is listening on PORT  http://localhost:3000");
 })
